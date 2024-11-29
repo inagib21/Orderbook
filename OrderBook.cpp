57df5f517d47eadcd3fdade7e0ac1b94369a6f60
inagib21 <inagib21@gmail.com>
2024-11-29 02:16:12 -0500

    add quantities

--- OrderBook.cpp
+++ OrderBook.cpp
@@ -48,7 +48,7 @@ public:
     OrderbookLevelInfos(const LevelInfos& bids, const LevelInfos& asks)
     : bids_{ bids }
     , asks_{ asks }
-    {}
+    { }
 
     const LevelInfos& GetBids() const { return bids_;}
     const LevelInfos& GetAsks() const { return asks_;}
@@ -62,8 +62,34 @@ public:
 class Order
 {
     public:
-        Order
-};
+        Order(OrderType orderType, OrderId orderId, Side side, Price price, Quantity quantity )
+        : orderType_{orderType}
+        , orderId_{orderId}
+        , side_{side}
+        , price_{price}
+        , initialQuantity_{quantity}
+        , remainingQuantity{quantity}
+        { }
+
+        OrderId GetOrderId() const {return orderId_;}
+        Side GetSide() const {return side_;}
+        Price GetPrice() const {return price_;}
+        OrderType GetOrderType() const {return orderType_;}
+        Quantity GetInitialQuantity() const { return initialQuantity_;}
+        Quantity GetRemainingQuantity() const {return remainingQuantity_; }
+        Quantity GetFilledQuantity() const {return GetInitialQuantity()- GetRemainingQuantity();}
+        void Fill( Quantity quantity)
+        {
+            if(quantity > GetRemainingQuantity())
+                throw std:: logic_error(std:: format("Order ({}) cannor be filled for more than its remaining quantity", GetOrderId()));
+
+                remainingQuantity -= quantity;
+        }
+
+
+        
+        
+        };
 
 
 int main()
